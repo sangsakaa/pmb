fc74e494a27461eaedbd9c7c3812942a8ca95206
sangsakaa <zainuri717@gmail.com>
2023-02-25 09:06:12 +0700

    npm run build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'K UI') }}</title>
-
+    <title>PMB UNIWA @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!-- Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
     <style>
@@ -24,25 +23,16 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div
-        x-data="mainState"
-        :class="{ dark: isDarkMode }"
-        x-on:resize.window="handleWindowResize"
-        x-cloak
-    >
+    <div x-data="mainState" :class="{ dark: isDarkMode }" x-on:resize.window="handleWindowResize" x-cloak>
         <div class="min-h-screen text-gray-900 bg-gray-100 dark:bg-dark-eval-0 dark:text-gray-200">
             <!-- Sidebar -->
             <x-sidebar.sidebar />
 
             <!-- Page Wrapper -->
-            <div
-                class="flex flex-col min-h-screen"
-                :class="{
+            <div class="flex flex-col min-h-screen" :class="{
                     'lg:ml-64': isSidebarOpen,
                     'md:ml-16': !isSidebarOpen
-                }"
-                style="transition-property: margin; transition-duration: 150ms;"
-            >
+                }" style="transition-property: margin; transition-duration: 150ms;">
 
                 <!-- Navbar -->
                 <x-navbar />
@@ -65,4 +55,5 @@
         </div>
     </div>
 </body>
+
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>PMB UNIWA @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
     <style>
@@ -24,12 +25,7 @@
 </head>
 
 <body>
-    <div
-        x-data="mainState"
-        class="font-sans antialiased"
-        :class="{dark: isDarkMode}"
-        x-cloak
-    >
+    <div x-data="mainState" class="font-sans antialiased" :class="{dark: isDarkMode}" x-cloak>
         <div class="flex flex-col min-h-screen text-gray-900 bg-gray-100 dark:bg-dark-eval-0 dark:text-gray-200">
             {{ $slot }}
 
@@ -37,26 +33,13 @@
         </div>
 
         <div class="fixed top-10 right-10">
-            <x-button
-                type="button"
-                icon-only
-                variant="secondary"
-                sr-text="Toggle dark mode"
-                x-on:click="toggleTheme"
-            >
-                <x-heroicon-o-moon
-                    x-show="!isDarkMode"
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                />
+            <x-button type="button" icon-only variant="secondary" sr-text="Toggle dark mode" x-on:click="toggleTheme">
+                <x-heroicon-o-moon x-show="!isDarkMode" aria-hidden="true" class="w-6 h-6" />
 
-                <x-heroicon-o-sun
-                    x-show="isDarkMode"
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                />
+                <x-heroicon-o-sun x-show="isDarkMode" aria-hidden="true" class="w-6 h-6" />
             </x-button>
         </div>
     </div>
 </body>
+
 </html>
