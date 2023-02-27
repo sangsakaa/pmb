3c9facf44a503f1035b92ef838216f09a1881f22
sangsakaa <zainuri717@gmail.com>
2023-02-28 00:05:55 +0700

    form formulir pendaftaran

--- resources/views/formulir/create.blade.php
+++ resources/views/formulir/create.blade.php
@@ -9,67 +9,240 @@
         </div>
     </x-slot>
 
-    <div class="p-2 sm:p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-        @role('mahasiswa')
-        <div class=" grid grid-cols-2 sm:grid-cols-4">
-            <div>Nama Mahasiswa</div>
-            <div> : {{$user->name}}</div>
-            <div>NIK</div>
-            <div> : {{$user->nomor_induk_penduduk}}</div>
-        </div>
-        <hr class="  border-2 mb-1 sm:mb-4   ">
-        <form action="" method="post">
+    @role('mahasiswa')
+    <form action="" method="post">
+        <div class=" grid gap-1">
+            <div class="p-1 sm:p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <div class=" grid grid-cols-1">
+                    <span class=" text-center uppercase font-semibold">Formulir Calon Mahasiswa</span>
+                </div>
+                <hr class="  border-2 mb-1 sm:mb-4   ">
+                <div class=" grid grid-cols-1 sm:grid-cols-2 gap-1 sm:gap-1">
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Tanggal Pendaftaran</label>
+                        </div>
+                        <div class=" ">
+                            <input type="date" name="tanggal_pendafraran" id="" class=" w-full py-1 px-1 " placeholder=" NISN : Nomor Induk Siswa Nasional" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di ijazah</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Tahun Akademi</label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
 
-            <div class=" grid grid-cols-1 sm:grid-cols-2 gap-1 sm:gap-1">
-                <div class=" w-full  gap-2">
-                    <div>
-                        <label for="" class="">Nama Lengkap </label>
+                                <option value="">2023/2024</option>
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
                     </div>
-                    <div class=" ">
-                        <input type="text" name="" id="" class=" w-full py-1 " placeholder=" Masukan Nama Sesui ID " required>
-                        <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas : KTP,SIM,IJAZAH</small>
-                    </div>
-                </div>
-                <div class=" w-full  gap-2">
-                    <div>
-                        <label for="" class="">Jenis Kelamin </label>
-                    </div>
-                    <div class="  ">
-                        <select name="jenis_kelamin" id="" class="  py-1 w-full">
-                            <option value="">Pilih Jenis Kelamin</option>
-                            <option value="">Laki Laki</option>
-                            <option value="">Perempuan</option>
-                        </select>
-                        <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
-                    </div>
-                </div>
-                <div class=" w-full  gap-2">
-                    <div>
-                        <label for="" class="">Jenis Kelamin </label>
-                    </div>
-                    <div class="  ">
-                        <select name="agama" id="" class=" py-1 w-full">
-                            <option value="">Pilih Agama</option>
-                            <option value="">Islam</option>
-                            <option value="">Kristen</option>
-                            <option value="">Budha</option>
-                        </select>
-                        <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
-                    </div>
-                </div>
-                <div class=" w-full  gap-2">
-                    <div class="  ">
-                        <a href="create-formulir" class=" bg-red-600  text-white px-3 py-1"> Batal</a>
-                        <button class=" bg-red-600  text-white px-1 py-1"> simpan</button>
-                    </div>
-                </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Gelombang Pendaftaran</label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
 
+                                <option value="">Gelombang I</option>
+                                <option value="">Gelombang II</option>
+                                <option value="">Gelombang III</option>
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Jenis Seleksi</label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
+                                <option value="">Reguler</option>
+                                <option value="">Beasiswa</option>
+
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </form>
-        @endrole
-    </div>
+            <div class="p-2 sm:p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <div class=" grid grid-cols-1">
+                    <div class=" grid grid-cols-1">
+                        <span class=" text-center uppercase font-semibold">BIODATA DIRI</span>
+                    </div>
+                </div>
+                <hr class="  border-2 mb-1 sm:mb-4   ">
+                <div class=" grid grid-cols-1 sm:grid-cols-2 gap-1 sm:gap-1">
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Nama Lengkap </label>
+                        </div>
+                        <div class=" ">
+                            <input type="text" name="" id="" class=" w-full py-1 px-1 " placeholder=" Masukan Nama Sesui ID " required>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas : KTP,SIM,IJAZAH</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Jenis Kelamin </label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
+                                <option value="">Pilih Jenis Kelamin</option>
+                                <option value="">Laki Laki</option>
+                                <option value="">Perempuan</option>
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Agama</label>
+                        </div>
+                        <div class="  ">
+                            <select name="agama" id="" class=" py-1 w-full">
+                                <option value="">Pilih Agama</option>
+                                <option value="">Islam</option>
+                                <option value="">Kristen</option>
+                                <option value="">Budha</option>
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Tempat Lahir </label>
+                        </div>
+                        <div class=" ">
+                            <input type="text" name="" id="" class=" w-full py-1 px-1 " placeholder=" Tempat Lahir sesui dokumen" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di KK KTP sejenisnya </small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Tanggal Lahir </label>
+                        </div>
+                        <div class=" ">
+                            <input type="date" name="" id="" class=" w-full py-1 px-1 " placeholder=" Tempat Lahir sesui dokumen" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di KK KTP sejenisnya </small>
+                        </div>
+                    </div>
 
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Jenis Indentitas</label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
+                                <option value="">KTP</option>
+                                <option value="">SIM</option>
 
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">NIK </label>
+                        </div>
+                        <div class=" ">
+                            <input type="text" name="" id="" class=" w-full py-1 px-1 " placeholder=" NISN : Nomor Induk Siswa Nasional" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di ijazah</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">NISN </label>
+                        </div>
+                        <div class=" ">
+                            <input type="text" name="" id="" class=" w-full py-1 px-1 " placeholder=" NISN : Nomor Induk Siswa Nasional" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di ijazah</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Tempat Lahir </label>
+                        </div>
+                        <div class=" ">
+                            <input type="text" name="" id="" class=" w-full py-1 px-1 " placeholder=" Tempat Lahir sesui dokumen" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di KK KTP sejenisnya </small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Tanggal Lahir </label>
+                        </div>
+                        <div class=" ">
+                            <input type="date" name="" id="" class=" w-full py-1 px-1 " placeholder=" Tempat Lahir sesui dokumen" required>
+                            <small class=" text-red-600">Wajib diisi sesui yang tertera di KK KTP sejenisnya </small>
+                        </div>
+                    </div>
 
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Jenis Kelamin </label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
+                                <option value="">Pilih Jenis Kelamin</option>
+                                <option value="">Laki Laki</option>
+                                <option value="">Perempuan</option>
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Program Studi Pilih Ke 1 </label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
+                                <option value="">Pilih Program Studi</option>
+                                <option value="">Teknik Informatika</option>
+                                <option value="">Teknik Industri</option>
+                                <option value="">Teknik Sipil</option>
+
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Program Studi Pilih Ke 2 </label>
+                        </div>
+                        <div class="  ">
+                            <select name="jenis_kelamin" id="" class="  py-1 w-full">
+                                <option value="">Pilih Program Studi</option>
+                                <option value="">Teknik Informatika</option>
+                                <option value="">Teknik Industri</option>
+                                <option value="">Teknik Sipil</option>
+
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+                    <div class=" w-full  gap-2">
+                        <div>
+                            <label for="" class="">Jenis Kelamin </label>
+                        </div>
+                        <div class="  ">
+                            <select name="agama" id="" class=" py-1 w-full">
+                                <option value="">Pilih Agama</option>
+                                <option value="">Islam</option>
+                                <option value="">Kristen</option>
+                                <option value="">Budha</option>
+                            </select>
+                            <small class=" text-red-600">Wajib diisi sesui Kartu Indentitas</small>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </form>
+    @endrole
 
 </x-app-layout>
