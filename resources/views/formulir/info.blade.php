6fea2350c300956c7abd69a5d97dd43be80d83c8
sangsakaa <zainuri717@gmail.com>
2023-03-09 05:38:35 +0700

    uploud

--- resources/views/formulir/info.blade.php
+++ resources/views/formulir/info.blade.php
@@ -47,12 +47,7 @@
 
     </div>
     @role('super admin')
-    <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-        {{ __("You're logged in!")  }}
-        <p>
-            <span> {{Auth::user()->name}}</span>
-        </p>
-    </div>
+
     @endrole
 
     @role('mahasiswa')
